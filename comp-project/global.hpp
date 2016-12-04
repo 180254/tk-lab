86b67c7f56d5176b59c19eac528f2801aaed784f
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-12-04 18:13:30 +0100

    update

--- comp-project/global.hpp
+++ comp-project/global.hpp
@@ -12,7 +12,8 @@
 #include "main.hpp"
 #include "node.hpp"
 #include "error.hpp"
-#include "parser.hpp"
+// #include "parser.hpp"
+#include "parser.m.hpp"
 #include "lexer.hpp"
 
 #define CP_DEBUG 1
